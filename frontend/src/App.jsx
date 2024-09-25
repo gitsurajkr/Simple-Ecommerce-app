cc707c2613a5641a6c727a54e9958825ec2d9125
Suraj Kumar <kumarsuraj.islampur@gmail.com>
2024-09-25 16:02:30 +0530

    Added several Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,10 +7,28 @@ import Sidebar from './page/AdminDashboard/Sidebar'
 import GridCard from './page/AdminDashboard/GridCard'
 import Button from './components/Button'
 import DeleteButton from './components/DeleteButton'
-
+// import ProductCard from './components/ProductCard'
+import ManageProducts from './page/ManageProduct'
+import AddProduct from './page/AddProduct'
+import InventoryManagement from './page/InventoryManagement'
+import OrderTable from './page/OrderTable'
+import CompanyTable from './page/CompanyTable'
+import UserManagement from './page/UserManagement'
+import { useState } from 'react'
 function App() {
+
+  
+    const [products, setProducts] = useState([
+      { name: 'Laptop', price: '999.99', stock: '50', company: 'TechCorp', category: 'Electronics' },
+      { name: 'Smartphone', price: '499.99', stock: '100', company: 'MobiTech', category: 'Electronics' },
+      { name: 'Headphones', price: '99.99', stock: '200', company: 'AudioPro', category: 'Audio' }
+    ]);
+
+
   return (
     <>
+
+
       {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navigate to="/signin" />} />
@@ -24,7 +42,29 @@ function App() {
         {/* <GridCard /> */}
       {/* </div> */}
       {/* <Button /> */}
-      <DeleteButton />
+      {/* <DeleteButton /> */}
+      {/* <ProductCard />  */}
+      {/* <BrowserRouter>
+      <Routes>
+        {/* Redirect from "/" to "/manage-products" */}
+        {/* <Route */}
+          {/* path="/" */}
+          {/* element={<Navigate to="/manage-products" />} */}
+        {/* /> */}
+        {/* <Route */}
+          {/* path="/manage-products" */}
+          {/* element={<ManageProducts products={products} setProducts={setProducts} />} */}
+        {/* /> */}
+        {/* <Route */}
+          {/* // </>path="/add-product" */}
+          {/* element={<AddProduct products={products} setProducts={setProducts} />} */}
+        {/* /> */}
+      {/* </Routes> */}
+    {/* </BrowserRouter> */} 
+    {/* <InventoryManagement /> */}
+    {/* <OrderTable /> */}
+    {/* <CompanyTable /> */}
+    <UserManagement />
     </>
   )
 }
--- frontend/src/components/ProductCard.jsx
+++
@@ -1,7 +0,0 @@
-function ProductCard({name, price, stock, company, category, Action}){
-return (
-    <>
-        <div className="flex justify-between items c"></div>
-    </>
-)
-}
